df1d1f7193603a397e457586bfadc6bb08d27239
Andres Chavez <andres.chavez@toshibagcs.com>
2020-10-11 23:12:50 -0500

    eslit was configured

--- models/models.js
+++ models/models.js
@@ -1,108 +1,109 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
 
-let CustomerSchema = new schema({
-    ClientNumber: String,
-    Name: String,
-    LastName: String,
-    Address: String,
-    City: String,
-    State: String,
-    Zipcode: String,
-    PhoneHome: String,
-    MobilePhone: String,
-    WorkPhone: String,
-    Status: {
-        type: String,
-        enum : ['active', 'deleted'],
-        default: 'active'
-    },  
-    Birthdate: Date,
-    Sex: {
-        type: String,
-        enum : ['M', 'F']
-    },
-    PointsEarned: Number  
-})
+const { Schema } = mongoose;
 
-let DepartmentSchema = new schema({
-    Name : String,
-    Description : String,
+const CustomerSchema = new Schema({
+  ClientNumber: String,
+  Name: String,
+  LastName: String,
+  Address: String,
+  City: String,
+  State: String,
+  Zipcode: String,
+  PhoneHome: String,
+  MobilePhone: String,
+  WorkPhone: String,
+  Status: {
+    type: String,
+    enum: ['active', 'deleted'],
+    default: 'active',
+  },
+  Birthdate: Date,
+  Sex: {
+    type: String,
+    enum: ['M', 'F'],
+  },
+  PointsEarned: Number,
 });
 
-let DepartmentGroupSchema = new schema({
-    Name : String,
-    Description : String,
-    Department : DepartmentSchema
-})
-
-let PaymentTypeSchema = new schema({
-    Name : String,
-    Description : String
+const DepartmentSchema = new Schema({
+  Name: String,
+  Description: String,
 });
 
-let ItemTypeSchema = new schema({
-    Type: String,
-    AgeNeeded: Boolean
+const DepartmentGroupSchema = new Schema({
+  Name: String,
+  Description: String,
+  Department: DepartmentSchema,
 });
 
-let ItemSchema = new schema({
-    ShortDescription: String,
-    UnitPrice: Number,
-    SpecialPrice: Number,
-    OfferQuantity: Number,
-    ItemType: ItemTypeSchema,
-    Department: DepartmentSchema,
-    Weightable: Boolean,
-    PriceRequired: Boolean,
-    Inventory: Number,
-    AllowFoodstamps: Boolean,
-    QuantityAllowed: Number,
-    QuantityRequired: Number,
-    AuthorizedSale: Boolean,
-    AllowPoints: Boolean,
-    ItemCost: Number,
-    Merchant: String,
-    Submerchant: String,
-    Barcode: Number,
-    LongDescription: String
+const PaymentTypeSchema = new Schema({
+  Name: String,
+  Description: String,
 });
 
-let PaymentSchema = new schema({
-    PaymentNumber: Number,
-    PaymentType: PaymentTypeSchema,
-    Amount: Number,
-    Status: {
-        type: String,
-        enum : ['completed', 'pending', 'canceled'],
-        default: 'pending'
-    }
+const ItemTypeSchema = new Schema({
+  Type: String,
+  AgeNeeded: Boolean,
 });
 
-let OrderSchema = new schema({
-    OrderNumber: Number,
-    Date: Date,
-    CustomerId: Number,
-    AwardPoints: Number,
-    UsedPoints: Number,
-    Status: {
-        type: String,
-        enum : ['completed', 'pending', 'active', 'canceled'],
-        default: 'pending'
-    },
-    Items: [ItemSchema],
-    Payments: [PaymentSchema],
-    TotalItems: Number,
-    TotalPayments: Number
+const ItemSchema = new Schema({
+  ShortDescription: String,
+  UnitPrice: Number,
+  SpecialPrice: Number,
+  OfferQuantity: Number,
+  ItemType: ItemTypeSchema,
+  Department: DepartmentSchema,
+  Weightable: Boolean,
+  PriceRequired: Boolean,
+  Inventory: Number,
+  AllowFoodstamps: Boolean,
+  QuantityAllowed: Number,
+  QuantityRequired: Number,
+  AuthorizedSale: Boolean,
+  AllowPoints: Boolean,
+  ItemCost: Number,
+  Merchant: String,
+  Submerchant: String,
+  Barcode: Number,
+  LongDescription: String,
+});
+
+const PaymentSchema = new Schema({
+  PaymentNumber: Number,
+  PaymentType: PaymentTypeSchema,
+  Amount: Number,
+  Status: {
+    type: String,
+    enum: ['completed', 'pending', 'canceled'],
+    default: 'pending',
+  },
+});
+
+const OrderSchema = new Schema({
+  OrderNumber: Number,
+  Date,
+  CustomerId: Number,
+  AwardPoints: Number,
+  UsedPoints: Number,
+  Status: {
+    type: String,
+    enum: ['completed', 'pending', 'active', 'canceled'],
+    default: 'pending',
+  },
+  Items: [ItemSchema],
+  Payments: [PaymentSchema],
+  TotalItems: Number,
+  TotalPayments: Number,
 });
 
 module.exports = {
-    CustomerSchema,
-    DepartmentSchema,
-    DepartmentGroupSchema,
-    PaymentTypeSchema,
-    ItemTypeSchema,
-    ItemSchema,
-    PaymentSchema,
-    OrderSchema
-}
+  CustomerSchema,
+  DepartmentSchema,
+  DepartmentGroupSchema,
+  PaymentTypeSchema,
+  ItemTypeSchema,
+  ItemSchema,
+  PaymentSchema,
+  OrderSchema,
+};
