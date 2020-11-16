8f89da21f4bfe198ac18814b00eebc98a818e0d3
andreschabb <andres.chavez@wizeline.com>
2020-11-16 13:54:52 -0600

    dummy test

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ mongoose.connect('mongodb://localhost/blockchain2', {
 mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
+
 // eslint-disable-next-line no-console
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
