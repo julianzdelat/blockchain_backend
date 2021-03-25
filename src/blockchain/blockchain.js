2ec74618405efdabfd1b24708902151c01ebe1c8
andreschabb <andres.chavez@wizeline.com>
2021-03-24 19:02:50 -0600

    lint errors fixed

--- src/blockchain/blockchain.js
+++ src/blockchain/blockchain.js
@@ -1,19 +1,18 @@
 const Block = require('./block');
 
 class Blockchain {
+  constructor() {
+    this.chain = [Block.genesis()];
+  }
 
-    constructor() {
-        this.chain = [Block.genesis()];
-    }
+  addBlock({ data }) {
+    const newBlock = Block.mineBlock({
+      lastBlock: this.chain[this.chain.length - 1],
+      data,
+    });
 
-    addBlock({ data }) {
-        const newBlock = Block.mineBlock({
-            lastBlock: this.chain[this.chain.length-1],
-            data
-        });
-
-        this.chain.push(newBlock);
-    }
+    this.chain.push(newBlock);
+  }
 }
 
 module.exports = Blockchain;
